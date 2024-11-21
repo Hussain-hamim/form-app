f3ccdea401d0670d29429368a07832c6ebc7a9f7
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-11-21 15:45:12 +0430

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+// https://docs.expo.dev/guides/using-eslint/
+// module.exports = {
+//   extends: 'expo',
+//   ignorePatterns: ['/dist/*'],
+// };
+
+module.exports = {
+  extends: ["expo", "prettier"],
+  plugins: ["prettier", "react-native"],
+  rules: {
+    "prettier/prettier": "error",
+    "react-native/no-unused-styles": "error",
+  },
+};
