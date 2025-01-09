1eafcacd453d28b24f2dee692446bbe78261526b
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2025-01-09 17:33:15 +0430

    git init form-app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,42 +1,14 @@
 import { Stack } from "expo-router";
-import { useEffect } from "react";
-import { Platform } from "react-native";
-// import * as QuickAction from "expo-quick-actions";
-// import { useQuickActionRouting } from "expo-quick-actions/router";
-
-// SplashScreen.preventAutoHideAsync();
+import React from "react";
+import { StatusBar } from "react-native";
 
 export default function Layout() {
-  // useQuickActionRouting();
-  // useEffect(() => {
-  //   QuickAction.setItems([
-  //     {
-  //       title: "Add a plant",
-  //       icon: "leaf",
-  //       id: "0",
-  //       params: { href: "/new" },
-  //     },
-  //   ]);
-  // }, []);
-
   return (
-    <Stack>
-      <Stack.Screen
-        name="(tabs)"
-        options={{ headerShown: false, animation: "fade" }}
-      />
-      <Stack.Screen
-        name="onboarding"
-        options={{ headerShown: false, animation: "fade" }}
-      />
-      <Stack.Screen
-        name="new"
-        options={{
-          presentation: "modal",
-          title: "New Plant",
-          gestureDirection: "vertical",
-        }}
-      />
-    </Stack>
+    <>
+      <StatusBar backgroundColor="wheat" />
+      <Stack>
+        <Stack.Screen name="index" options={{ title: "Home" }} />
+      </Stack>
+    </>
   );
 }
